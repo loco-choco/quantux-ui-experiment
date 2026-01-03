7f3e1a838d91cebaef9fc0d84df3954a64062a91
loco-choco <contact@locochoco.dev>
2026-01-03 03:55:52 +0100

    refactored a lot of the item logic

--- inventories/grid/inventory.gd
+++ inventories/grid/inventory.gd
@@ -1,40 +1,15 @@
-extends Control
+extends PanelContainer
 
-const inventory_item_scene = preload("res://inventories/grid/inventory_slot.tscn")
-#const inventory_item = preload("res://inventories/grid/inventory_item.tscn")
+@export var items: Array[ItemData] = []
+@export var inventory_item_scene: PackedScene
+@onready var item_grid: GridContainer = $Bag
 
-@export var rows: int = 3
-@export var columns: int = 3
-
-var slots: Array[InventorySlot]
-
-var selected_item: Item = null
-var original_owner: InventorySlot = null
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in range(columns * rows):
-		var slot : InventorySlot = inventory_item_scene.instantiate()
-		slots.append(slot)
-		$CenterContainer/GridContainer.add_child(slot)
-		$CenterContainer/GridContainer.columns = columns
-		slot.slot_input.connect(_on_slot_input)
+	for i in items:
+		add_item(i)
 
-func _on_slot_input(which: InventorySlot) -> void:
-	if not selected_item and which.has_item():
-		selected_item = which.release_item()
-		original_owner = which
-	elif selected_item:
-		if which != original_owner:
-			original_owner.aquire_item(which.release_item())
-		which.aquire_item(selected_item)
-		selected_item = null
-		original_owner = null
-
-func add_item(item: Item) -> bool:
-	for slot in slots:
-		if not slot.has_item():
-			item.hide_in_game()
-			slot.aquire_item(item)
-			return true
-	return false
+func add_item(item_data: ItemData) -> void:
+	var inventory_item = inventory_item_scene.instantiate()
+	inventory_item.data = item_data
+	add_child(inventory_item)
+	var success = item_grid.attempt_to_add_item_data(inventory_item)
---
+++ items/inventory_item.gd
@@ -0,0 +1,37 @@
+class_name InventoryItem extends Sprite2D
+
+const ITEM_SIZE = 16
+
+var data: ItemData = null
+var is_picked: bool = false
+
+var size: Vector2:
+	get():
+		return Vector2(data.dimensions.x, data.dimensions.y) * ITEM_SIZE
+		
+var anchor_point: Vector2:
+	get():
+		return global_position + offset
+
+func _ready() -> void:
+	#offset = - size / 2
+	if data:
+		texture = data.texture
+
+func _process(_delta: float) -> void:
+	if is_picked:
+		global_position = get_global_mouse_position()
+		
+func set_init_position(pos: Vector2i) -> void:
+	global_position = pos + Vector2i(size / 2)
+	
+func get_picked_up() -> void:
+	add_to_group("held_item")
+	is_picked = true
+	z_index = 10
+
+func get_placed(pos: Vector2i) -> void:
+	is_picked = false
+	z_index = 0
+	global_position = pos + Vector2i(size / 2)
+	remove_from_group("held_item")
