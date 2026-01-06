68c63fb9258b09db4b9090badc782995a3097f80
loco-choco <contact@locochoco.dev>
2026-01-06 23:16:13 +0100

    made inventory ui scale with screen

--- inventories/grid/inventory.gd
+++ inventories/grid/inventory.gd
@@ -1,7 +1,7 @@
 extends PanelContainer
 
 @export var inventory_item_scene: PackedScene
-@onready var item_grid: GridContainer = $Bag
+@onready var item_grid: GridContainer = $MarginContainer/AspectRatioContainer/PanelContainer/MarginContainer/HBoxContainer/AspectRatioContainer/Bag
 
 func add_item(item_data: ItemData) -> bool:
 	var inventory_item = inventory_item_scene.instantiate()
