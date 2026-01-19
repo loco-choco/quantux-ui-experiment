eaa3a43740bd0d5902e28f90aa1cd446dfd1d960
loco-choco <contact@locochoco.dev>
2026-01-20 00:18:09 +0100

    added selection indicator and better logic

--- scripts/ui/inventory/radial_inventory/radial_inventory.gd
+++ scripts/ui/inventory/radial_inventory/radial_inventory.gd
@@ -6,13 +6,22 @@ signal menu_closed()
 
 @onready var entries : Container = $%Entries
 
+var last_entry : RadialInventoryEntry = null
+
 func _process(_delta: float) -> void:
 	var selection_vec: Vector2 = (get_local_mouse_position() - size/2 ).normalized()
 	if selection_vec.is_zero_approx():
 		return
 	var selection: int = floori(rad_to_deg(selection_vec.angle() + PI) / 360 \
 						 * entries.get_child_count()) % entries.get_child_count()
-	if Input.is_action_just_pressed("item_use"):
+	var entry_selected : RadialInventoryEntry = entries.get_child(selection) as RadialInventoryEntry
+	if last_entry != entry_selected:
+		if last_entry:
+			last_entry.show_diselected()
+		entry_selected.show_selected()
+		last_entry = entry_selected
+	
+	if Input.is_action_just_pressed("inventory_select"):
 		print("Selected: ", selection)
 		menu_closed.emit()
 
@@ -21,6 +30,7 @@ func update_radial_inventory(items: Array[InventoryItem]) -> void:
 	create_entries(items)
 
 func clean_entries() -> void:
+	last_entry = null
 	for c in entries.get_children():
 		c.queue_free()
 
